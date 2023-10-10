7e65ea5e13ff8b6a284750467a8d945e9e0ec1c7
ALAN LINHARES DA SILVA <ALAN0173@DIGITALCOLLEGE.LOCAL>
2023-10-10 19:42:09 -0300

    criacao do filtro no search

--- src/components/ToolBar/index.jsx
+++ src/components/ToolBar/index.jsx
@@ -1,9 +1,10 @@
-export default function ToolBar({ handleSort }) {
+export default function ToolBar({ handleSort, handleSearch }) {
   return (
     <div className="mb-8 flex justify-between items-center border py-5 px-3 rounded-md bg-white shadow">
       <h2 className="uppercase text-2xl font-bold">Produtos</h2>
       <div className=" flex gap-8 items-center">
         <input
+          onChange={(e) => handleSearch(e.target.value)}
           className="border-2 outline-none rounded-md py-1 px-2"
           type="text"
           placeholder="Pesquise aqui..."
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -2,19 +2,18 @@ import { useEffect, useState } from "react";
 import Table from "../../components/Table";
 import axios from "axios";
 import ToolBar from "../../components/ToolBar";
+import { useProducts } from "../../hooks/useProducts";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [limit, setLimit] = useState(5);
   const [sort, setSort] = useState("asc");
+  const [search, setSearch] = useState("");
+  const { list } = useProducts();
 
   async function getAllProducts() {
-    const response = await axios.get(
-      `https://fakestoreapi.com/products?limit=${limit}&sort=${sort}`
-    );
-    if (response.data) {
-      setProducts(response.data);
-    }
+    setProducts(await list(limit, sort));
   }
 
   function handleLimit(value) {
@@ -25,6 +24,18 @@ export default function Products() {
     setSort(value);
   }
 
+  function handleSearch(value) {
+    setSearch(value);
+  }
+  function filterProducts() {
+    const filterWord = search.toLowerCase();
+    const newProducts = products.filter((product) => {
+      const productTitle = product.title.toLowerCase();
+      return productTitle.includes(filterWord);
+    });
+    setFilteredProducts(newProducts);
+  }
+
   useEffect(() => {
     getAllProducts();
   }, []);
@@ -33,10 +44,17 @@ export default function Products() {
     getAllProducts();
   }, [limit, sort]);
 
+  useEffect(() => {
+    filterProducts();
+  }, [search]);
+
   return (
     <>
-      <ToolBar handleSort={handleSort} />
-      <Table products={products} handleLimit={handleLimit} />
+      <ToolBar handleSort={handleSort} handleSearch={handleSearch} />
+      <Table
+        products={search ? filteredProducts : products} //operador ternario
+        handleLimit={handleLimit}
+      />
     </>
   );
 }
